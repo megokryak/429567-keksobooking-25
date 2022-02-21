c09d6fabb008a49c2f513865934c2143eab4c136
XaJIaC@yandex.ru <XaJIaC@yandex.ru>
2022-02-22 00:06:02 +0300

    Исправил

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 //Функция возвращает рандомное целое число из диапазона включительно
 function getRandomInt(min, max) {
+  if (min < 0 || max <0)
+    throw new Error('negative value')
+
   if (min > max) {
-    let storage = min;
-    min = max;
-    max = storage;
+    [min, max] = [max, min];
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,15 +12,15 @@ function getRandomInt(min, max) {
 
 //функция возвращает рандомное число с запятое из диапазона включительно. передается точность после запятой
 function getRandomFloat (min, max, accuracy) {
-  if (min > max) {
-    let storage = min;
-    min = max;
-    max = storage;
-  }
+  if (min < 0 || max <0)
+    throw new Error('negative float value')
 
-  return Math.floor(Math.random() * (max - min + 1) * Math.pow(10, accuracy)) / Math.pow(10, accuracy) + min;
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return getRandomInt (Math.floor(min * Math.pow(10,accuracy)), Math.floor(max * Math.pow(10, accuracy))) / Math.pow(10, accuracy);
 }
 
 
 console.log(getRandomInt(100, 20));
-console.log(getRandomFloat(250, 200, 2));
+console.log(getRandomFloat(200, 201, 2));
