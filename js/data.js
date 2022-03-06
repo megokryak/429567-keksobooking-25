95871ce8c4e5df30b97a64ccb5a053ee893bdcf8
XaJIaC@yandex.ru <XaJIaC@yandex.ru>
2022-03-06 11:44:07 +0300

    организация кода

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const RENT_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const RENT_CHECKIN = ['12:00', '13:00', '14:00'];
+const RENT_CHECKOUT = ['12:00', '13:00', '14:00'];
+const RENT_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const RENT_FOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const MIN_LAT = 35.65000;
+const MAX_LAT = 35.70000;
+const MIN_LNG = 139.70000;
+const MAX_LNG = 139.80000;
+const MAX_ADS = 10;
+const MAX_GUEST = 5;
+const MAX_ROOMS = 7;
+const MAX_PRICE = 22800;
+const ads = [];
+
+export {
+  RENT_TYPE,
+  RENT_CHECKIN,
+  RENT_CHECKOUT,
+  RENT_FEATURES,
+  RENT_FOTOS,
+  MIN_LAT,
+  MAX_LAT,
+  MIN_LNG,
+  MAX_LNG,
+  MAX_ADS,
+  MAX_GUEST,
+  MAX_ROOMS,
+  MAX_PRICE,
+  ads
+};
---
+++ js/util.js
@@ -0,0 +1,38 @@
+//Функция возвращает рандомное целое число из диапазона включительно
+
+function getRandomInt(min, max) {
+  if (min < 0 || max < 0) {
+    throw new Error('negative value');
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//функция возвращает рандомное число с запятое из диапазона включительно. передается точность после запятой
+function getRandomFloat (min, max, accuracy) {
+  if (min < 0 || max <0) {
+    throw new Error('negative float value');
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return getRandomInt (Math.floor(min * Math.pow(10,accuracy)), Math.floor(max * Math.pow(10, accuracy))) / Math.pow(10, accuracy);
+}
+
+//взята с https://learn.javascript.ru/task/shuffle
+function shuffle(arrayInput) {
+  const maxElementArray = getRandomInt(1, arrayInput.length - 1);
+  const array = arrayInput.slice();
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+
+    // поменять элементы местами
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array.slice(maxElementArray);
+}
+
+export {getRandomInt, getRandomFloat, shuffle};
